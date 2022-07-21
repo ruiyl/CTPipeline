399e2e3fa8316a44ea158735663c39128acaa74a
ruiyl <ruiyl1@hotmail.com>
2022-07-21 20:50:09 +0100

    feat: add simple character control

--- Assets/Scripts/CharacterMono.cs
+++ Assets/Scripts/CharacterMono.cs
@@ -5,15 +5,27 @@ namespace Assets.Scripts
 {
 	public class CharacterMono : MonoBehaviour
 	{
+		[SerializeField] private Rigidbody rb;
+		[SerializeField] private float speed;
+		[SerializeField] private float rotationMaxSpeed;
 
-		// Use this for initialization
-		void Start()
+		private Quaternion targetRot;
+
+		private void FixedUpdate()
 		{
+			Vector3 inputDir = new Vector3((Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0), 0f,
+				(Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0));
 
+			Vector3 offset = inputDir.normalized * speed;
+			rb.velocity = offset;
+			if (inputDir.magnitude > 0f)
+			{
+				targetRot = Quaternion.LookRotation(inputDir.normalized, Vector3.up);
+				rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRot, rotationMaxSpeed);
+			}
 		}
 
-		// Update is called once per frame
-		void Update()
+		private void OnTriggerEnter(Collider other)
 		{
 
 		}
